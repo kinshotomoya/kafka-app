f6158b4dda0554f5ad56633368e2f7462f80eb18
金正朋也 <mhtnim1023@gmail.com>
2020-01-30 01:04:21 +0900

    add wordcount app

--- src/main/java/myapps/LineSplit.java
+++ src/main/java/myapps/LineSplit.java
@@ -1,6 +1,5 @@
 package myapps;
 
-import org.apache.kafka.clients.KafkaClient;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -8,35 +7,26 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class Pipe {
-    public static void main(String[] args) throws Exception{
+public class LineSplit {
+    public static void main(String[] args) throws Exception {
         Properties props = new Properties();
-        // kafkaライブラリを入れた時点で、StreamsConfigが設定されている
-        // そこに設定を加えている
-        // streamを一意に識別する
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        // kafkaサーバ
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-linesplit");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        // シリアライズするライブラリを指定
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
-        // topic streams-plaintext-inputと接続するsource streamを作っている
-        // 返り値として、key valueの値がstreams-plaintext-input topicから返ってくる
-        // KStream<Object, Object> stream = builder.stream("streams-plaintext-input")
-        // toまで一行で書ける
-        // streams-plaintext-input topicから取得して、streams-pipe-output
-        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        source.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .to("streams-linesplit-output");
+
         final Topology topology = builder.build();
-        System.out.println(topology.describe());
-
-        // kafka clientを生成する
-        KafkaStreams streams = new KafkaStreams(topology, props);
-
+        final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
         // control-cが押された場合にハンドリングする
--- src/main/java/myapps/WordCount.java
+++ src/main/java/myapps/WordCount.java
@@ -1,42 +1,42 @@
 package myapps;
 
-import org.apache.kafka.clients.KafkaClient;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.*;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class Pipe {
-    public static void main(String[] args) throws Exception{
+public class WordCount {
+    public static void main(String[] args) {
         Properties props = new Properties();
-        // kafkaライブラリを入れた時点で、StreamsConfigが設定されている
-        // そこに設定を加えている
-        // streamを一意に識別する
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        // kafkaサーバ
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        // シリアライズするライブラリを指定
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        final StreamsBuilder builder = new StreamsBuilder();
-        // topic streams-plaintext-inputと接続するsource streamを作っている
-        // 返り値として、key valueの値がstreams-plaintext-input topicから返ってくる
-        // KStream<Object, Object> stream = builder.stream("streams-plaintext-input")
-        // toまで一行で書ける
-        // streams-plaintext-input topicから取得して、streams-pipe-output
-        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        // ("ken", "japan america"), ("bob", "france, italy")
+        // keyを複製して、valueにflatMapを適応させて、
+        // => ("ken", "japan"), ("ken", "america"), ("bob", "france"), ("bob", "italy")にする
+        // valueの方を取り出して、mapで処理する
+        source.flatMapValues((ValueMapper<String, Iterable<String>>) value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
+                .groupBy((key, value) -> value)
+                .count(Materialized.as("counts-store"))
+                .toStream()
+                .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+
         final Topology topology = builder.build();
         System.out.println(topology.describe());
-
-        // kafka clientを生成する
-        KafkaStreams streams = new KafkaStreams(topology, props);
-
+        final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
         // control-cが押された場合にハンドリングする
